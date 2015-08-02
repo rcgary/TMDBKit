37ecaacc0c3a1e94d38d40970e7b3b9c7abdb47f
Chao Ruan <chaoruan818@gmail.com>
2015-08-02 15:33:33 +1000

    Add reactivecocao to parse

--- TMDBKit/TMDBPageResponse+Parse.h
+++ TMDBKit/TMDBPageResponse+Parse.h
@@ -5,7 +5,7 @@
 //  Created by Chao Ruan on 1/08/2015.
 //  Copyright (c) 2015 Chao Ruan. All rights reserved.
 //
-
+#import <ReactiveCocoa/ReactiveCocoa.h>
 #import "TMDBPageResponse.h"
 
 @interface TMDBPageResponse (Parse)
