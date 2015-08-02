86f6693b73317b5871f642d6da524bce0124807d
Chao Ruan <chaoruan818@gmail.com>
2015-08-02 21:27:43 +1000

    Fix image bugs

--- TMDBKit/TMDBImageResponse.h
+++ TMDBKit/TMDBImageResponse.h
@@ -6,9 +6,9 @@
 //  Copyright (c) 2015 Chao Ruan. All rights reserved.
 //
 
-#import "MTLModel.h"
+#import <Mantle/Mantle.h>
 
-@interface TMDBImageResponse : MTLModel
+@interface TMDBImageResponse : MTLModel <MTLJSONSerializing>
 @property (nonatomic, copy, readonly) NSArray *backdrops;
 @property (nonatomic, copy, readonly) NSArray *posters;
 @end
